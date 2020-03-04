dfa931e6816b4c57ab2ed994bbfd705fb805b649
trapq3dU <49921218+trapq3dU@users.noreply.github.com>
2020-03-04 20:14:48 +0530

    Create More_Linked_Lists.java

---
+++ More_Linked_Lists.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+class Node{
+	int data;
+	Node next;
+	Node(int d){
+        data=d;
+        next=null;
+    }
+	
+}
+class More_Linked_Lists
+{
+
+    public static Node removeDuplicates(Node head) {
+      //Write your code here
+        Node cnode=head;
+        while(cnode!=null && cnode.next!=null)
+        {
+            Node n=cnode;
+            while(n.next!=null)
+            {
+                if(n.next.data==cnode.data)
+                {
+                    Node nn=n.next.next;
+                    Node temp=n.next;
+                    n.next=nn;
+                    temp=null;
+                }
+                else
+                {
+                    n=n.next;
+                }
+            }
+            cnode=cnode.next;
+        }
+        return head;
+
+    }
+
+	 public static  Node insert(Node head,int data)
+     {
+        Node p=new Node(data);			
+        if(head==null)
+            head=p;
+        else if(head.next==null)
+            head.next=p;
+        else
+        {
+            Node start=head;
+            while(start.next!=null)
+                start=start.next;
+            start.next=p;
+
+        }
+        return head;
+    }
+    public static void display(Node head)
+        {
+              Node start=head;
+              while(start!=null)
+              {
+                  System.out.print(start.data+" ");
+                  start=start.next;
+              }
+        }
+        public static void main(String args[])
+        {
+              Scanner sc=new Scanner(System.in);
+              Node head=null;
+              int T=sc.nextInt();
+              while(T-->0){
+                  int ele=sc.nextInt();
+                  head=insert(head,ele);
+              }
+              head=removeDuplicates(head);
+              display(head);
+
+       }
+    }
